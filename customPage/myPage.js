d1e311be7f4118c85fbb74a003ba237289fb6b26
Utkarsh Kumar <utkarsh18e@iitg.ac.in>
2021-07-12 21:03:07 +0530

    form with toggle feature added

--- customPage/myPage.js
+++ customPage/myPage.js
@@ -1,4 +1,17 @@
-function setup(){
-    createCanvas(200,200);
-    background(0);
-}
+// function setup(){
+//     createCanvas(200,200);
+//     background(0);
+// }
+
+$(function(){
+    $('h1').click(function(){
+        console.log("I am clicked!!!");
+    });
+    $('.userForms').hide();
+    $('#toggleForm').click(function(){
+        console.log("I am also clicked!!!");
+        $('.userForms').slideToggle( "slow", function() {
+            // Animation complete.
+          });
+    });
+});
