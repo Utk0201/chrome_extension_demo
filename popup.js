fe01b713d8374a45abb892b0cde28d4f7742185b
Utkarsh Kumar <utkarsh18e@iitg.ac.in>
2021-07-15 17:34:22 +0530

    code simplified

--- popup.js
+++ popup.js
@@ -7,23 +7,28 @@ $(function () {
         })
     });
 
+
+    var timerWindow = $(".set-time");
+    var workWindow = $(".work-window");
+    var breakWindow = $(".break-window");
+    var workInput = document.querySelector(".work-time");
+    var breakInput = document.querySelector(".break-time");
+    const resetAll = document.querySelector('.all-reset');
+    var bodyContent = document.querySelector("body");
+    const setTimings = document.querySelector(".set-work-break");
+    const pauseWorkBtn = document.querySelector('.work-pause');
+    const takeBreakBtn = document.querySelector('.take-break');
+
     resetWindow();
 
     function resetWindow() {
-        //  buttons        
-        const timerForm = $(".set-time");
-        const workWindow = $(".work-window");
-        const breakWindow = $(".break-window");
         // by default , work and break windows are hidden
-        workWindow.hide("fast");
-        breakWindow.hide("fast");
-        timerForm.show("slow");
+        workWindow.hide("slow");
+        breakWindow.hide("slow");
+        timerWindow.show("slow");
         //  contents shown
-        const myBtn = document.querySelector(".set-work-break");
-        myBtn.addEventListener("click", (e) => {
+        setTimings.addEventListener("click", (e) => {
             e.preventDefault();
-            var workInput = document.querySelector(".work-time");
-            var breakInput = document.querySelector(".break-time");
             // to ensure the input is not blank
             // if(!workInput.value || !breakInput.value){
             //     console.log("Please enter a value");
@@ -34,28 +39,25 @@ $(function () {
     }
 
     function doWork(workDuration) {
-        //  buttons        
-        const timerForm = $(".set-time");
-        const workWindow = $(".work-window");
-        timerForm.slideUp("slow");
+        timerWindow.slideUp("slow");
         workWindow.show("slow");
         // buttons
-        const resetAll = document.querySelector('.all-reset');
         resetAll.addEventListener("click", resetWindow);
-        const pauseWork = document.querySelector('.work-pause');
-        pauseWork.addEventListener("click", (e)=>{
+        pauseWorkBtn.addEventListener("click", (e) => {
             e.preventDefault();
             console.log("Pausing work");
         });
-        const takeBreak = document.querySelector('.take-break');
-        takeBreak.addEventListener("click",(e)=>{
-            e.preventDefault();
-            console.log("Taking a break!!");
-        });
+        takeBreakBtn.addEventListener("click", takeBreak);
         //////////////////////////////////////////
         console.log("Shifted to work mode for ", workDuration);
-        var bodyContent = document.querySelector("body");
         console.log(bodyContent.style);
         bodyContent.style.backgroundImage = "linear-gradient(45deg, rgba(0,0,0,0.2), white), url('https://i.pinimg.com/564x/63/00/a7/6300a74eae90be4938bb39257e3fe6d9.jpg')";
+    }
+
+    function takeBreak(e) {
+        e.preventDefault();
+        // workWindow.hide("slow");
+        // timerWindow.show("slow");
+        console.log("Taking a break!!");
     }
 });
